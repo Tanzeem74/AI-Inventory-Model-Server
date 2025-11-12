d015f28a054d3ab0edcb69f81e47d60174feea8e
Tanzeem <tanzeemafsar74@gmail.com>
2025-11-13 00:04:20 +0600

    adding task for deployment

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const verifyFirebaseToken = async (req, res, next) => {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    //await client.connect();
     const db = client.db('Model_db');
     const modelCollection = db.collection('model');
     const purchaseCollection = db.collection('purchase');
@@ -147,7 +147,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    //await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
